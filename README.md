004a231328626dcaf0f374eb12601b514be1a8a4
Saurav Suman <sauravsuman@Sauravs-MacBook-Pro.local>
2021-04-24 19:55:19 +0530

    Added code to check prime number

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python-scripts
 Repository for all python code and scripts
+
+*Python is one of the diverse OOPs language which can be used to write code for many use cases. It finds it's application in building frontend and backend applications, data science, automation etc.This repository will be maintained to have various codes which can used in all these areas.*
+
---
+++ check-prime-number.py
@@ -0,0 +1,18 @@
+#Take a number input from a user to check whether it's prime or not
+num = input("Enter a Integer number to check if it's prime or not = ")
+
+mark = False
+
+if num > 1:
+    for i in range(2,num):
+        if num % i == 0:
+            # If the number is divisble then it's not prime number and break the loop.
+            mark = True
+            break
+
+#If the check mark is True then it's not a prime number else it's a prime number
+if mark:
+    print("The Input number {} is a not a prime number".format(num))
+else:
+    print("The Input number {} is a prime number".format(num))
+
