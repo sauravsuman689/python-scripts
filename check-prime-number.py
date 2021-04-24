9b31d3f29ee6007e5b3c86320c9bbb162560bcbf
Saurav Suman <sauravsuman689@gmail.com>
2021-04-24 20:18:43 +0530

    Uopdated prime number code

--- check-prime-number.py
+++ check-prime-number.py
@@ -1,18 +1,26 @@
+#Program to check if the number is prime or not.
+
 #Take a number input from a user to check whether it's prime or not..!
 num = input("Enter a Integer number to check if it's prime or not = ")
 
 mark = False
 
-if num > 1:
-    for i in range(2,num):
-        if num % i == 0:
-            # If the number is divisble then it's not prime number and break the loop.
-            mark = True
-            break
+if type(num) is int:
+
+    if num > 1:
+        for i in range(2,num):
+            if num % i == 0:
+                # If the number is divisble then it's not prime number and break the loop.
+                mark = True
+                break
+
 
 #If the check mark is True then it's not a prime number else it's a prime number
-if mark:
-    print("The Input number {} is a not a prime number".format(num))
+    if mark:
+        print("The Input number {} is a not a prime number".format(num))
+    else:
+        print("The Input number {} is a prime number".format(num))
+
 else:
-    print("The Input number {} is a prime number".format(num))
+    print("It seems you have enterred a float value,Please enter an Integer value..!")
 
